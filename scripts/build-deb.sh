10170ad3d6d2f6acf5a7da781778f960fad9934b
Vladimir Rachkin <vova@kpnn.ru>
2025-04-08 11:08:24 +0300

    Fix unbound GPDB_PKG_VERSION (#27)

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -116,7 +116,7 @@ if [ -z ${BUILD_USER+x} ]; then
   export BUILD_USER=github
 fi
 
-if [ -z ${GPDB_PKG_VERSION} ]; then
+if [ -z "${GPDB_PKG_VERSION+x}" ]; then
   export GPDB_PKG_VERSION=${GPDB_FULL_VERSION}-${BUILD_NUMBER}-yandex.$(git --git-dir=.git rev-list HEAD --count).$(git --git-dir=.git rev-parse --short HEAD)
 fi
 
