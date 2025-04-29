7c203dad0ebb7ab65df93732143fb45983e60e3c
Jaroslav Novikov <njrslv@qavm-f9b691f5.qemu>
2025-04-29 17:48:19 +0300

    remove unnecessary env var setting

--- build_automation/gpdb/scripts/test-gpdb.sh
+++ build_automation/gpdb/scripts/test-gpdb.sh
@@ -56,7 +56,6 @@
 set -euo pipefail
 
 export BUILD_DESTINATION="/opt/greenplum-db-6"
-export PGOPTIONS="${PGOPTIONS:--c optimizer=on}"
 
 # Source common utilities
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
